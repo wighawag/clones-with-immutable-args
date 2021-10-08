8699e31a4e36e65d7bb43a63cc9f54d63c9a82a2
wighawag <wighawag@gmail.com>
2021-10-08 19:06:44 +0100

    cleanup

--- src/ClonesWithCallData.sol
+++ src/ClonesWithCallData.sol
@@ -21,8 +21,8 @@ contract ClonesWithCallData is DSTest {
       // CREATION (11 bytes)
       // -------------------------------------------------------------------------------------------------------------
 
-      // 3d          | RETURNDATASIZE        | 0	                      | –
-      // 61 runtime  | PUSH2 runtime (r)     | r 0	                    | –
+      // 3d          | RETURNDATASIZE        | 0                       | –
+      // 61 runtime  | PUSH2 runtime (r)     | r 0                     | –
       mstore(
         ptr,
         0x3d61000000000000000000000000000000000000000000000000000000000000
@@ -30,12 +30,12 @@ contract ClonesWithCallData is DSTest {
       mstore(add(ptr, 0x02), shl(240, runSize)) // size of the contract running bytecode (16 bits)
 
       // creation size = 0b
-      // 80	         | DUP1	                 | r r 0	                  | –
-      // 60 creation | PUSH1 creation (c)    | c r r 0	                | –
-      // 3d	         | RETURNDATASIZE	       | 0 c r r 0	              | –
-      // 39	         | CODECOPY	             | r 0	                    | [0-2d]: runtime code
-      // 81	         | DUP2                  | 0 c  0                   | [0-2d]: runtime code
-      // f3	         | RETURN                | 0	                      | [0-2d]: runtime code
+      // 80          | DUP1                  | r r 0                   | –
+      // 60 creation | PUSH1 creation (c)    | c r r 0                 | –
+      // 3d          | RETURNDATASIZE        | 0 c r r 0               | –
+      // 39          | CODECOPY              | r 0                     | [0-2d]: runtime code
+      // 81          | DUP2                  | 0 c  0                  | [0-2d]: runtime code
+      // f3          | RETURN                | 0                       | [0-2d]: runtime code
       mstore(
         add(ptr, 0x04),
         0x80600b3d3981f300000000000000000000000000000000000000000000000000
@@ -45,11 +45,11 @@ contract ClonesWithCallData is DSTest {
       // RUNTIME
       // -------------------------------------------------------------------------------------------------------------
 
-      // 36	         | CALLDATASIZE	         | cds	                    | –
-      // 3d  	       | RETURNDATASIZE	       | 0 cds	                  | –
-      // 3d	         | RETURNDATASIZE	       | 0 0 cds	                | –
-      // 37	         | CALLDATACOPY	         | –                        | [0, cds] = calldata
-      // 61	         | PUSH2 extra           | extra	                  | [0, cds] = calldata
+      // 36          | CALLDATASIZE          | cds                     | –
+      // 3d          | RETURNDATASIZE        | 0 cds                   | –
+      // 3d          | RETURNDATASIZE        | 0 0 cds                 | –
+      // 37          | CALLDATACOPY          | –                       | [0, cds] = calldata
+      // 61          | PUSH2 extra           | extra                   | [0, cds] = calldata
        mstore(
         add(ptr, 0x0b),
         0x363d3d3761000000000000000000000000000000000000000000000000000000
@@ -59,15 +59,14 @@ contract ClonesWithCallData is DSTest {
         shl(240, extraLength)
       )
 
-      // 60	0x38     | PUSH1 0x38            | 0x38 extra               | [0, cds] = calldata // 0x38 (56) is runtime size - data
-      // 36	         | CALLDATASIZE	         | cds 0x38 extra           | [0, cds] = calldata
-      // 39          | CODECOPY              | _                        | [0, cds] = calldata
-      // 3d	         | RETURNDATASIZE	       | 0	                      | [0, cds] = calldata
-      // 3d	         | RETURNDATASIZE	       | 0 0	                    | [0, cds] = calldata
-      // 3d	         | RETURNDATASIZE	       | 0 0 0	                  | [0, cds] = calldata
-      // 36	         | CALLDATASIZE	         | cds 0 0 0	              | [0, cds] = calldata
-      // 61 extra    | PUSH2 extra           | extra cds 0 0 0          | [0, cds] = calldata
-     
+      // 60 0x38     | PUSH1 0x38            | 0x38 extra              | [0, cds] = calldata // 0x38 (56) is runtime size - data
+      // 36          | CALLDATASIZE          | cds 0x38 extra          | [0, cds] = calldata
+      // 39          | CODECOPY              | _                       | [0, cds] = calldata
+      // 3d          | RETURNDATASIZE        | 0                       | [0, cds] = calldata
+      // 3d          | RETURNDATASIZE        | 0 0                     | [0, cds] = calldata
+      // 3d          | RETURNDATASIZE        | 0 0 0                   | [0, cds] = calldata
+      // 36          | CALLDATASIZE          | cds 0 0 0               | [0, cds] = calldata
+      // 61 extra    | PUSH2 extra           | extra cds 0 0 0         | [0, cds] = calldata
       mstore(
         add(ptr, 0x12),
         0x603836393d3d3d36610000000000000000000000000000000000000000000000
@@ -77,27 +76,27 @@ contract ClonesWithCallData is DSTest {
         shl(240, extraLength)
       )
 
-      // 01          | ADD                   | cds+extra 0 0 0          | [0, cds] = calldata
-      // 3d	         | RETURNDATASIZE	       | 0 cds 0 0 0	            | [0, cds] = calldata
-      // 73 addr	   | PUSH20 0x123…	       | addr 0 cds 0 0 0	        | [0, cds] = calldata
+      // 01          | ADD                   | cds+extra 0 0 0         | [0, cds] = calldata
+      // 3d          | RETURNDATASIZE        | 0 cds 0 0 0             | [0, cds] = calldata
+      // 73 addr     | PUSH20 0x123…         | addr 0 cds 0 0 0        | [0, cds] = calldata
       mstore(add(ptr, 0x1d), 0x013d7300000000000000000000000000000000000000000000000000000000)
       mstore(add(ptr, 0x20), shl(0x60, implementation))
 
 
-      // 5a	         | GAS	                 | gas addr 0 cds 0 0 0	    | [0, cds] = calldata
-      // f4	         | DELEGATECALL	         |success 0	                | [0, cds] = calldata
-      // 3d	         | RETURNDATASIZE	       | rds success 0	          | [0, cds] = calldata
-      // 82	         | DUP3	                 | 0 rds success 0	        | [0, cds] = calldata
-      // 80	         | DUP1	                 | 0 0 rds success 0	      | [0, cds] = calldata
-      // 3e	         | RETURNDATACOPY    	   | success 0	              | [0, rds] = return data (there might be some irrelevant leftovers in memory [rds, cds] when rds < cds)
-      // 90	         | SWAP1	               | 0 success	              | [0, rds] = return data
-      // 3d	         | RETURNDATASIZE        | rds 0 success	          | [0, rds] = return data
-      // 91          | SWAP2	               | success 0 rds	          | [0, rds] = return data
-      // 60 0x36	   | PUSH1 0x36            | 0x36 sucess 0 rds	      | [0, rds] = return data
-      // 57	         | JUMPI	               | 0 rds	                  | [0, rds] = return data
-      // fd	         | REVERT	               | –	                      | [0, rds] = return data
-      // 5b	         | JUMPDEST	             | 0 rds	                  | [0, rds] = return data
-      // f3	         | RETURN	               | –	                      | [0, rds] = return data
+      // 5a          | GAS                   | gas addr 0 cds 0 0 0    | [0, cds] = calldata
+      // f4          | DELEGATECALL          |success 0                | [0, cds] = calldata
+      // 3d          | RETURNDATASIZE        | rds success 0           | [0, cds] = calldata
+      // 82          | DUP3                  | 0 rds success 0         | [0, cds] = calldata
+      // 80          | DUP1                  | 0 0 rds success 0       | [0, cds] = calldata
+      // 3e          | RETURNDATACOPY        | success 0               | [0, rds] = return data (there might be some irrelevant leftovers in memory [rds, cds] when rds < cds)
+      // 90          | SWAP1                 | 0 success               | [0, rds] = return data
+      // 3d          | RETURNDATASIZE        | rds 0 success           | [0, rds] = return data
+      // 91          | SWAP2                 | success 0 rds           | [0, rds] = return data
+      // 60 0x36     | PUSH1 0x36            | 0x36 sucess 0 rds       | [0, rds] = return data
+      // 57          | JUMPI                 | 0 rds                   | [0, rds] = return data
+      // fd          | REVERT                | –                       | [0, rds] = return data
+      // 5b          | JUMPDEST              | 0 rds                   | [0, rds] = return data
+      // f3          | RETURN                | –                       | [0, rds] = return data
 
       mstore(
         add(ptr, 0x34),
@@ -125,7 +124,6 @@ contract ClonesWithCallData is DSTest {
     assembly {
       mstore(copyPtr, and(mload(dataPtr), mask))
     }
-
     assembly {
       instance := create(0, ptr, creationSize)
     }
--- src/ClonesWithImmutableArgs.sol
+++ src/ClonesWithImmutableArgs.sol
@@ -21,8 +21,8 @@ contract ClonesWithImmutableArgs is DSTest {
       // CREATION (11 bytes)
       // -------------------------------------------------------------------------------------------------------------
 
-      // 3d          | RETURNDATASIZE        | 0	                      | –
-      // 61 runtime  | PUSH2 runtime (r)     | r 0	                    | –
+      // 3d          | RETURNDATASIZE        | 0                       | –
+      // 61 runtime  | PUSH2 runtime (r)     | r 0                     | –
       mstore(
         ptr,
         0x3d61000000000000000000000000000000000000000000000000000000000000
@@ -30,12 +30,12 @@ contract ClonesWithImmutableArgs is DSTest {
       mstore(add(ptr, 0x02), shl(240, runSize)) // size of the contract running bytecode (16 bits)
 
       // creation size = 0b
-      // 80	         | DUP1	                 | r r 0	                  | –
-      // 60 creation | PUSH1 creation (c)    | c r r 0	                | –
-      // 3d	         | RETURNDATASIZE	       | 0 c r r 0	              | –
-      // 39	         | CODECOPY	             | r 0	                    | [0-2d]: runtime code
-      // 81	         | DUP2                  | 0 c  0                   | [0-2d]: runtime code
-      // f3	         | RETURN                | 0	                      | [0-2d]: runtime code
+      // 80          | DUP1                  | r r 0                   | –
+      // 60 creation | PUSH1 creation (c)    | c r r 0                 | –
+      // 3d          | RETURNDATASIZE        | 0 c r r 0               | –
+      // 39          | CODECOPY              | r 0                     | [0-2d]: runtime code
+      // 81          | DUP2                  | 0 c  0                  | [0-2d]: runtime code
+      // f3          | RETURN                | 0                       | [0-2d]: runtime code
       mstore(
         add(ptr, 0x04),
         0x80600b3d3981f300000000000000000000000000000000000000000000000000
@@ -45,16 +45,16 @@ contract ClonesWithImmutableArgs is DSTest {
       // RUNTIME
       // -------------------------------------------------------------------------------------------------------------
 
-      // 36	         | CALLDATASIZE	         | cds	                    | –
-      // 3d  	       | RETURNDATASIZE	       | 0 cds	                  | –
-      // 3d	         | RETURNDATASIZE	       | 0 0 cds	                | –
-      // 37	         | CALLDATACOPY	         | –                        | [0, cds] = calldata
-      // 3d	         | RETURNDATASIZE	       | 0	                      | [0, cds] = calldata
-      // 3d	         | RETURNDATASIZE	       | 0 0	                    | [0, cds] = calldata
-      // 3d	         | RETURNDATASIZE	       | 0 0 0	                  | [0, cds] = calldata
-      // 36	         | CALLDATASIZE	         | cds 0 0 0	              | [0, cds] = calldata
-      // 3d	         | RETURNDATASIZE	       | 0 cds 0 0 0	            | [0, cds] = calldata
-      // 73 addr	   | PUSH20 0x123…	       | addr 0 cds 0 0 0	        | [0, cds] = calldata
+      // 36          | CALLDATASIZE          | cds                     | –
+      // 3d          | RETURNDATASIZE        | 0 cds                   | –
+      // 3d          | RETURNDATASIZE        | 0 0 cds                 | –
+      // 37          | CALLDATACOPY          | –                       | [0, cds] = calldata
+      // 3d          | RETURNDATASIZE        | 0                       | [0, cds] = calldata
+      // 3d          | RETURNDATASIZE        | 0 0                     | [0, cds] = calldata
+      // 3d          | RETURNDATASIZE        | 0 0 0                   | [0, cds] = calldata
+      // 36          | CALLDATASIZE          | cds 0 0 0               | [0, cds] = calldata
+      // 3d          | RETURNDATASIZE        | 0 cds 0 0 0             | [0, cds] = calldata
+      // 73 addr     | PUSH20 0x123…         | addr 0 cds 0 0 0        | [0, cds] = calldata
       mstore(
         add(ptr, 0x0b),
         0x363d3d373d3d3d363d7300000000000000000000000000000000000000000000
@@ -62,20 +62,20 @@ contract ClonesWithImmutableArgs is DSTest {
       mstore(add(ptr, 0x15), shl(0x60, implementation))
 
 
-      // 5a	         | GAS	                 | gas addr 0 cds 0 0 0	    | [0, cds] = calldata
-      // f4	         | DELEGATECALL	         |success 0	                | [0, cds] = calldata
-      // 3d	         | RETURNDATASIZE	       | rds success 0	          | [0, cds] = calldata
-      // 82	         | DUP3	                 | 0 rds success 0	        | [0, cds] = calldata
-      // 80	         | DUP1	                 | 0 0 rds success 0	      | [0, cds] = calldata
-      // 3e	         | RETURNDATACOPY    	   | success 0	              | [0, rds] = return data (there might be some irrelevant leftovers in memory [rds, cds] when rds < cds)
-      // 90	         | SWAP1	               | 0 success	              | [0, rds] = return data
-      // 3d	         | RETURNDATASIZE        | rds 0 success	          | [0, rds] = return data
-      // 91          | SWAP2	               | success 0 rds	          | [0, rds] = return data
-      // 60 dest	   | PUSH1 dest            | dest sucess 0 rds	      | [0, rds] = return data
-      // 57	         | JUMPI	               | 0 rds	                  | [0, rds] = return data
-      // fd	         | REVERT	               | –	                      | [0, rds] = return data
-      // 5b	         | JUMPDEST	             | 0 rds	                  | [0, rds] = return data
-      // f3	         | RETURN	               | –	                      | [0, rds] = return data
+      // 5a          | GAS                   | gas addr 0 cds 0 0 0    | [0, cds] = calldata
+      // f4          | DELEGATECALL          |success 0                | [0, cds] = calldata
+      // 3d          | RETURNDATASIZE        | rds success 0           | [0, cds] = calldata
+      // 82          | DUP3                  | 0 rds success 0         | [0, cds] = calldata
+      // 80          | DUP1                  | 0 0 rds success 0       | [0, cds] = calldata
+      // 3e          | RETURNDATACOPY        | success 0               | [0, rds] = return data (there might be some irrelevant leftovers in memory [rds, cds] when rds < cds)
+      // 90          | SWAP1                 | 0 success               | [0, rds] = return data
+      // 3d          | RETURNDATASIZE        | rds 0 success           | [0, rds] = return data
+      // 91          | SWAP2                 | success 0 rds           | [0, rds] = return data
+      // 60 dest     | PUSH1 dest            | dest sucess 0 rds       | [0, rds] = return data
+      // 57          | JUMPI                 | 0 rds                   | [0, rds] = return data
+      // fd          | REVERT                | –                       | [0, rds] = return data
+      // 5b          | JUMPDEST              | 0 rds                   | [0, rds] = return data
+      // f3          | RETURN                | –                       | [0, rds] = return data
 
       mstore(
         add(ptr, 0x29),
