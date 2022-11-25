2525fc6dd2318c0131ab8d61f6aa35d1b5daab02
Saw-mon-and-Natalie <samangharib50233@gmail.com>
2022-11-25 01:57:47 -0800

    removed unnecessary unchecked block.

--- src/ClonesWithImmutableArgs.sol
+++ src/ClonesWithImmutableArgs.sol
@@ -22,140 +22,136 @@ library ClonesWithImmutableArgs {
         returns (address payable instance)
     {
         // unrealistic for memory ptr or data length to exceed 256 bits
-        unchecked {
-            // solhint-disable-next-line no-inline-assembly
-            assembly {
-                let extraLength := add(mload(data), 2) // +2 bytes for telling how much data there is appended to the call
-                let creationSize := add(extraLength, BOOTSTRAP_LENGTH)
-                let runSize := sub(creationSize, 0x0a)
+        // solhint-disable-next-line no-inline-assembly
+        assembly {
+            let extraLength := add(mload(data), 2) // +2 bytes for telling how much data there is appended to the call
+            let creationSize := add(extraLength, BOOTSTRAP_LENGTH)
+            let runSize := sub(creationSize, 0x0a)
 
-                // free memory pointer
-                let ptr := mload(FREE_MEMORY_POINTER_SLOT)
-                
-                // -------------------------------------------------------------------------------------------------------------
-                // CREATION (10 bytes)
-                // -------------------------------------------------------------------------------------------------------------
+            // free memory pointer
+            let ptr := mload(FREE_MEMORY_POINTER_SLOT)
+            
+            // -------------------------------------------------------------------------------------------------------------
+            // CREATION (10 bytes)
+            // -------------------------------------------------------------------------------------------------------------
 
-                // 61 runtime  | PUSH2 runtime (r)     | r                       | –
-                // 3d          | RETURNDATASIZE        | 0 r                     | –
-                // 81          | DUP2                  | r 0 r                   | –
-                // 60 offset   | PUSH1 offset (o)      | o r 0 r                 | –
-                // 3d          | RETURNDATASIZE        | 0 o r 0 r               | –
-                // 39          | CODECOPY              | 0 r                     | [0 - runSize): runtime code
-                // f3          | RETURN                |                         | [0 - runSize): runtime code
+            // 61 runtime  | PUSH2 runtime (r)     | r                       | –
+            // 3d          | RETURNDATASIZE        | 0 r                     | –
+            // 81          | DUP2                  | r 0 r                   | –
+            // 60 offset   | PUSH1 offset (o)      | o r 0 r                 | –
+            // 3d          | RETURNDATASIZE        | 0 o r 0 r               | –
+            // 39          | CODECOPY              | 0 r                     | [0 - runSize): runtime code
+            // f3          | RETURN                |                         | [0 - runSize): runtime code
 
-                // -------------------------------------------------------------------------------------------------------------
-                // RUNTIME (53 bytes + extraLength)
-                // -------------------------------------------------------------------------------------------------------------
+            // -------------------------------------------------------------------------------------------------------------
+            // RUNTIME (53 bytes + extraLength)
+            // -------------------------------------------------------------------------------------------------------------
 
-                // --- copy calldata to memmory ---
-                // 36          | CALLDATASIZE          | cds                     | –
-                // 3d          | RETURNDATASIZE        | 0 cds                   | –
-                // 3d          | RETURNDATASIZE        | 0 0 cds                 | –
-                // 37          | CALLDATACOPY          |                         | [0 - cds): calldata
+            // --- copy calldata to memmory ---
+            // 36          | CALLDATASIZE          | cds                     | –
+            // 3d          | RETURNDATASIZE        | 0 cds                   | –
+            // 3d          | RETURNDATASIZE        | 0 0 cds                 | –
+            // 37          | CALLDATACOPY          |                         | [0 - cds): calldata
 
-                // --- keep some values in stack ---
-                // 3d          | RETURNDATASIZE        | 0                       | [0 - cds): calldata
-                // 3d          | RETURNDATASIZE        | 0 0                     | [0 - cds): calldata
-                // 3d          | RETURNDATASIZE        | 0 0 0                   | [0 - cds): calldata
-                // 3d          | RETURNDATASIZE        | 0 0 0 0                 | [0 - cds): calldata
-                // 61 extra    | PUSH2 extra (e)       | e 0 0 0 0               | [0 - cds): calldata
+            // --- keep some values in stack ---
+            // 3d          | RETURNDATASIZE        | 0                       | [0 - cds): calldata
+            // 3d          | RETURNDATASIZE        | 0 0                     | [0 - cds): calldata
+            // 3d          | RETURNDATASIZE        | 0 0 0                   | [0 - cds): calldata
+            // 3d          | RETURNDATASIZE        | 0 0 0 0                 | [0 - cds): calldata
+            // 61 extra    | PUSH2 extra (e)       | e 0 0 0 0               | [0 - cds): calldata
 
-                // --- copy extra data to memory ---
-                // 80          | DUP1                  | e e 0 0 0 0             | [0 - cds): calldata
-                // 60 0x35     | PUSH1 0x35            | 0x35 e e 0 0 0 0        | [0 - cds): calldata
-                // 36          | CALLDATASIZE          | cds 0x35 e e 0 0 0 0    | [0 - cds): calldata
-                // 39          | CODECOPY              | e 0 0 0 0               | [0 - cds): calldata, [cds - cds + e): extraData
+            // --- copy extra data to memory ---
+            // 80          | DUP1                  | e e 0 0 0 0             | [0 - cds): calldata
+            // 60 0x35     | PUSH1 0x35            | 0x35 e e 0 0 0 0        | [0 - cds): calldata
+            // 36          | CALLDATASIZE          | cds 0x35 e e 0 0 0 0    | [0 - cds): calldata
+            // 39          | CODECOPY              | e 0 0 0 0               | [0 - cds): calldata, [cds - cds + e): extraData
 
-                // --- delegate call to the implementation contract ---
-                // 36          | CALLDATASIZE          | cds e 0 0 0 0           | [0 - cds): calldata, [cds - cds + e): extraData
-                // 01          | ADD                   | cds+e 0 0 0 0           | [0 - cds): calldata, [cds - cds + e): extraData
-                // 3d          | RETURNDATASIZE        | 0 cds+e 0 0 0 0         | [0 - cds): calldata, [cds - cds + e): extraData
-                // 73 addr     | PUSH20 addr           | addr 0 cds+e 0 0 0 0    | [0 - cds): calldata, [cds - cds + e): extraData
-                // 5a          | GAS                   | gas addr 0 cds+e 0 0 0 0| [0 - cds): calldata, [cds - cds + e): extraData
-                // f4          | DELEGATECALL          | success 0 0             | [0 - cds): calldata, [cds - cds + e): extraData
+            // --- delegate call to the implementation contract ---
+            // 36          | CALLDATASIZE          | cds e 0 0 0 0           | [0 - cds): calldata, [cds - cds + e): extraData
+            // 01          | ADD                   | cds+e 0 0 0 0           | [0 - cds): calldata, [cds - cds + e): extraData
+            // 3d          | RETURNDATASIZE        | 0 cds+e 0 0 0 0         | [0 - cds): calldata, [cds - cds + e): extraData
+            // 73 addr     | PUSH20 addr           | addr 0 cds+e 0 0 0 0    | [0 - cds): calldata, [cds - cds + e): extraData
+            // 5a          | GAS                   | gas addr 0 cds+e 0 0 0 0| [0 - cds): calldata, [cds - cds + e): extraData
+            // f4          | DELEGATECALL          | success 0 0             | [0 - cds): calldata, [cds - cds + e): extraData
 
-                // --- copy return data to memory ---
-                // 3d          | RETURNDATASIZE        | rds success 0 0         | [0 - cds): calldata, [cds - cds + e): extraData
-                // 3d          | RETURNDATASIZE        | rds rds success 0 0     | [0 - cds): calldata, [cds - cds + e): extraData
-                // 93          | SWAP4                 | 0 rds success 0 rds     | [0 - cds): calldata, [cds - cds + e): extraData
-                // 80          | DUP1                  | 0 0 rds success 0 rds   | [0 - cds): calldata, [cds - cds + e): extraData
-                // 3e          | RETURNDATACOPY        | success 0 rds           | [0 - rds): returndata, ... the rest might be dirty
-                
-                // 60 0x33     | PUSH1 0x33            | 0x33 success            | [0 - rds): returndata, ... the rest might be dirty
-                // 57          | JUMPI                 |                         | [0 - rds): returndata, ... the rest might be dirty
+            // --- copy return data to memory ---
+            // 3d          | RETURNDATASIZE        | rds success 0 0         | [0 - cds): calldata, [cds - cds + e): extraData
+            // 3d          | RETURNDATASIZE        | rds rds success 0 0     | [0 - cds): calldata, [cds - cds + e): extraData
+            // 93          | SWAP4                 | 0 rds success 0 rds     | [0 - cds): calldata, [cds - cds + e): extraData
+            // 80          | DUP1                  | 0 0 rds success 0 rds   | [0 - cds): calldata, [cds - cds + e): extraData
+            // 3e          | RETURNDATACOPY        | success 0 rds           | [0 - rds): returndata, ... the rest might be dirty
+            
+            // 60 0x33     | PUSH1 0x33            | 0x33 success 0 rds      | [0 - rds): returndata, ... the rest might be dirty
+            // 57          | JUMPI                 | 0 rds                   | [0 - rds): returndata, ... the rest might be dirty
 
-                // --- revert ---
-                // fd          | REVERT                |                         | [0 - rds): returndata, ... the rest might be dirty
+            // --- revert ---
+            // fd          | REVERT                |                         | [0 - rds): returndata, ... the rest might be dirty
 
-                // --- return ---
-                // 5b          | JUMPDEST              |                         | [0 - rds): returndata, ... the rest might be dirty
-                // f3          | RETURN                |                         | [0 - rds): returndata, ... the rest might be dirty
+            // --- return ---
+            // 5b          | JUMPDEST              | 0 rds                   | [0 - rds): returndata, ... the rest might be dirty
+            // f3          | RETURN                |                         | [0 - rds): returndata, ... the rest might be dirty
 
-                mstore(
-                    ptr,
+            mstore(
+                ptr,
+                or(
+                    hex"610000_3d_81_600a_3d_39_f3_36_3d_3d_37_3d_3d_3d_3d_610000_80_6035_36_39_36_01_3d_73",
                     or(
-                        hex"610000_3d_81_600a_3d_39_f3_36_3d_3d_37_3d_3d_3d_3d_610000_80_6035_36_39_36_01_3d_73",
-                        or(
-                            shl(0xe8, runSize),
-                            shl(0x58, extraLength)
-                        )
+                        shl(0xe8, runSize),
+                        shl(0x58, extraLength)
                     )
                 )
-                
-                mstore(
-                    add(ptr, 0x1e),
-                    shl(0x60, implementation)
-                )
-
-                mstore(
-                    add(ptr, 0x32),
-                    hex"5a_f4_3d_3d_93_80_3e_6033_57_fd_5b_f3"
-                )
-
-
-                // -------------------------------------------------------------------------------------------------------------
-                // APPENDED DATA (Accessible from extcodecopy)
-                // (but also send as appended data to the delegatecall)
-                // -------------------------------------------------------------------------------------------------------------
-
-                let counter := mload(data)
-                let copyPtr := add(ptr, BOOTSTRAP_LENGTH)
-                let dataPtr := add(data, ONE_WORD)
-
-                for {} true {} {
-                    if lt(counter, ONE_WORD) {
-                        break
-                    }
-
-                    mstore(copyPtr, mload(dataPtr))
-
-                    copyPtr := add(copyPtr, ONE_WORD)
-                    dataPtr := add(dataPtr, ONE_WORD)
-
-                    counter := sub(counter, ONE_WORD)
-                }
-                    
-                let mask := shl(
-                    mul(0x8, sub(ONE_WORD, counter)), 
-                    not(0)
-                )
-
-                mstore(copyPtr, and(mload(dataPtr), mask))
-                copyPtr := add(copyPtr, counter)
-                mstore(copyPtr, shl(0xf0, extraLength))
-
-                instance := create(0, ptr, creationSize)
-
-                // Update free memory pointer
-                mstore(FREE_MEMORY_POINTER_SLOT, add(ptr, creationSize))
-            }
-
+            )
             
+            mstore(
+                add(ptr, 0x1e),
+                shl(0x60, implementation)
+            )
 
-            if (instance == address(0)) {
-                revert CreateFail();
+            mstore(
+                add(ptr, 0x32),
+                hex"5a_f4_3d_3d_93_80_3e_6033_57_fd_5b_f3"
+            )
+
+
+            // -------------------------------------------------------------------------------------------------------------
+            // APPENDED DATA (Accessible from extcodecopy)
+            // (but also send as appended data to the delegatecall)
+            // -------------------------------------------------------------------------------------------------------------
+
+            let counter := mload(data)
+            let copyPtr := add(ptr, BOOTSTRAP_LENGTH)
+            let dataPtr := add(data, ONE_WORD)
+
+            for {} true {} {
+                if lt(counter, ONE_WORD) {
+                    break
+                }
+
+                mstore(copyPtr, mload(dataPtr))
+
+                copyPtr := add(copyPtr, ONE_WORD)
+                dataPtr := add(dataPtr, ONE_WORD)
+
+                counter := sub(counter, ONE_WORD)
             }
+                
+            let mask := shl(
+                mul(0x8, sub(ONE_WORD, counter)), 
+                not(0)
+            )
+
+            mstore(copyPtr, and(mload(dataPtr), mask))
+            copyPtr := add(copyPtr, counter)
+            mstore(copyPtr, shl(0xf0, extraLength))
+
+            instance := create(0, ptr, creationSize)
+
+            // Update free memory pointer
+            mstore(FREE_MEMORY_POINTER_SLOT, add(ptr, creationSize))
+        }
+
+        if (instance == address(0)) {
+            revert CreateFail();
         }
     }
 }
